fa453d8873b2dd47f6858e953f99e7e24aae1fe5
Richard Cumberland <richard@gingermess.com>
2021-12-06 13:45:38 +0000

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -18,7 +18,7 @@ class FishTracker:
             self.fish_by_timer_value[timer_value] = self.fish_by_timer_value.get(timer_value+1, 0)
         # spawn new fish
         self.fish_by_timer_value[8] = new_fish
-        # insert fish that spawned into timer 6
+        # insert fish that just had offspring into timer 6
         self.fish_by_timer_value[6] = self.fish_by_timer_value.get(6, 0) + new_fish
 
     def total_fish(self):
@@ -31,9 +31,13 @@ tracker = FishTracker({})
 # existing fish
 for existing_fish_timer in data:
     tracker.add_fish(existing_fish_timer)
-# go go gadget fish!
+# part 1 - 80 days
 days = 80
 for day in range(0, days):
     tracker.tick()
-
 print(f"Fish after {days} days: {tracker.total_fish()}")
+# part 2 - 256 days (actually 256-80)
+days = 256-80
+for day in range(0, days):
+    tracker.tick()
+print(f"Fish after 256 days: {tracker.total_fish()}")
