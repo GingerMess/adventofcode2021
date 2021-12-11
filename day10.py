f245956bc88d22c24fbc194c8e75accd0c674caa
Richard Cumberland <richard@gingermess.com>
2021-12-11 16:28:32 +0000

    Day 10 part 2

--- day10.py
+++ day10.py
@@ -1,12 +1,14 @@
+from math import floor
+
 import common
 
 
 def calculate_syntax_error(lines):
-    stack = []
     corrupted_total = 0
+    repair_scores = []
     for line in lines:
+        stack = []
         corrupted = 0
-        incomplete = False
         for char in line:
             if char in ['(', '[', '{', '<']:
                 stack.append(char)
@@ -29,12 +31,24 @@ def calculate_syntax_error(lines):
                     if expected_opposite != '<':
                         corrupted = 25137
                         break
-        if len(stack) > 0:
-            incomplete = True
         corrupted_total += corrupted
-    return corrupted_total
+        if corrupted > 0:
+            continue
+        if len(stack) > 0:
+            # line is incomplete
+            matching_brackets = {'(': ')', '[': ']', '{': '}', '<': '>'}
+            repair_string = list(reversed([matching_brackets[opener] for opener in stack]))
+            scores = {')': 1, ']': 2, '}': 3, '>': 4}
+            score = 0
+            for char in repair_string:
+                score = (score * 5) + (scores[char])
+            repair_scores.append(score)
+    return corrupted_total, repair_scores
 
 
 data = common.read_all_data("input/day10.txt")
-syntax_error = calculate_syntax_error(data)
-print(f"(Part 1) Total Syntax Error: {syntax_error}")
+corruption, repairs = calculate_syntax_error(data)
+sorted_repairs = sorted(repairs)
+middle_score = sorted_repairs[floor(len(sorted_repairs) / 2)]
+print(f"(Part 1) Total Syntax Error: {corruption}")
+print(f"(Part 2) Middle Repair Score: {middle_score}")
